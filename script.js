eb19aa51c07644c74e9dd8c27419282859c95f87
Gleice Kelly <gleice.kelly22@hotmail.com>
2023-12-06 11:14:30 -0300

    Alteração no button liga/desliga

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lamp = document.querySelector("#lamp");
-const btnLigar = document.querySelector("#ligado");
-const btnDesligar = document.querySelector("#desligado");
+const btnLigaDesliga = document.querySelector("#liga-desliga");
+/*const btnLigar = document.querySelector("#ligado");
+const btnDesligar = document.querySelector("#desligado"); */
 
 function verificaLampQuebrada () {
     return lamp.src.indexOf( 'quebrada' ) > -1; //procura se dentro do source da string tem a palavra 'quebrada'.
@@ -15,6 +16,7 @@ function lampadaAcesa () {
 function lampDesligada () {
     if (!verificaLampQuebrada () ) {
         lamp.src = "./assets/desligada.jpg";
+        btnLigaDesliga == "Ligar";
     }
 }
 
@@ -22,9 +24,20 @@ function lampQuebrada () {
     lamp.src = "./assets/quebrada.jpg";
 }
 
-btnLigar.addEventListener("click", lampadaAcesa);
-btnDesligar.addEventListener("click", lampDesligada);
-lamp.addEventListener("dblclick", lampQuebrada)
+function ligarDesligar () {
+    if(btnLigaDesliga.textContent == "Ligar") {
+        lampadaAcesa ();
+        btnLigaDesliga.textContent == "Desligar";
+    } else {
+        lampDesligada ();
+        btnLigaDesliga.textContent == "Ligar";
+    }
+}
+
+btnLigaDesliga.addEventListener("click", ligarDesligar);
+/*btnLigar.addEventListener("click", lampadaAcesa);
+btnDesligar.addEventListener("click", lampDesligada);*/
+lamp.addEventListener("dblclick", lampQuebrada);
 lamp.addEventListener("mouseover", lampadaAcesa);
 lamp.addEventListener("mouseleave", lampDesligada);
 
